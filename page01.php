9f8b704fd825ab644f178758b55b51c76649b980
Gabrielvrferreira <99758660+Gabrielvrferreira@users.noreply.github.com>
2022-04-06 16:41:10 -0300

    Create page01.php

---
+++ page01.php
@@ -0,0 +1,25 @@
+<?php
+
+//cria uma variável 
+$carro ="fusca";
+
+
+
+?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php echo "olá mundo!"?>
+
+<?php // isso aqui é uma imagem aleatória ?>
+
+
+</body>
+</html>
