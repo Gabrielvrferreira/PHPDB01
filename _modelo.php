a28472d7743ddf4a52af45e1e852c7e5b2fc88be
Gabrielvrferreira <99758660+Gabrielvrferreira@users.noreply.github.com>
2022-04-08 16:45:48 -0300

    aula 3

--- _modelo.php
+++ _modelo.php
@@ -1,5 +1,8 @@
+<?php 
 
-
+//inclui o cabeçalho da página 
+require('_header.php');
+?>
             <section></section>
             <h2>título da pagina</h2>
             <p>What is Lorem Ipsum?
@@ -11,18 +14,10 @@
             <p>Why do we use it?
                 It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
             <aside></aside>
-            </main>
-            <footer>
-              <a href="/" title="Página inicial"></a>><i class="fa-solid fa-house-chimney"></i></a>
-            </footer>
+            
+            <?php
+
+            require('_footer.php');
             
 
-        </main>
-
-        <footer></footer>
-
-    </div>
-
-</body>
-
-</html>
+        
