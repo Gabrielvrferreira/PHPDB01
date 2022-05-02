0fa219fd53046c4313293c99c1de0c797dcf3fb3
Gabrielvrferreira <99758660+Gabrielvrferreira@users.noreply.github.com>
2022-05-02 14:49:08 -0300

    aula 11

--- index.php
+++ index.php
@@ -1,88 +1,173 @@
-<?php
+-- Apaga o banco de dados caso exista.
+DROP DATABASE IF EXISTS vitugo;
 
-/**
- * Arquivo que faz a configuração incial da página.
- */
-require($_SERVER['DOCUMENT_ROOT'] . '/_config.php');
+-- Cria o banco de dados
+CREATE DATABASE vitugo CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-/***********************************************
- * Seus códigos PHP desta página iniciam aqui! *
- ***********************************************/
+-- Seleciona banco de dados.
+USE vitugo; 
 
-// Variável que armazena todos os artigos para exibição no HTML.
-$artigos = '';
+-- Cria tabela de usuários/autores.
+CREATE TABLE users (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_name VARCHAR(255),
+    user_email VARCHAR(255),
+    user_birth DATE,
+    user_photo VARCHAR(255),
+    user_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_profile TEXT,
+    user_password VARCHAR(255),
+    user_type ENUM('user', 'author', 'admin') DEFAULT 'user',
+    user_status ENUM('on', 'off', 'deleted') DEFAULT 'on'
+);
 
-// SQL que obtém todos os artigos.
-$sql = <<<SQL
+-- Cria tabela de artigos
+CREATE TABLE articles (
+    art_id INT PRIMARY KEY AUTO_INCREMENT,
+    art_title VARCHAR(127),
+    art_intro VARCHAR(255),
+    art_thumb VARCHAR(255),
+    art_content TEXT,
+    art_author INT,
+    art_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    art_status ENUM('on', 'off', 'deleted') DEFAULT 'on',
+    FOREIGN KEY (art_author) REFERENCES users (user_id)
+);
 
-SELECT art_id, art_title, art_intro, art_thumb 
-    FROM articles
-WHERE art_status = 'on'
-    AND art_date <= NOW()
-ORDER BY art_date DESC;
+-- Cria tabela de comentários nos artigos
+CREATE TABLE comments (
+    cmt_id INT PRIMARY KEY AUTO_INCREMENT,
+    cmt_article INT,
+    cmt_author INT,
+    cmt_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    cmt_status ENUM('on', 'off', 'deleted') DEFAULT 'on',
+    FOREIGN KEY (cmt_article) REFERENCES articles (art_id),
+    FOREIGN KEY (cmt_author) REFERENCES users (user_id)
+);
 
-SQL;
+-- Cria tabela com contatos do site
+CREATE TABLE contacts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(127),
+    email VARCHAR(255),
+    subject VARCHAR(255),
+    message TEXT,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status ENUM('on', 'off', 'deleted') DEFAULT 'on'
+);
 
-// Executa a query --> '$res' contém os artigos encontrados.
-$res = $conn->query($sql);
+-- Insere dados em 'users'
+INSERT INTO users (
+    user_name,
+    user_email,
+    user_birth,
+    user_photo,
+    user_profile,
+    user_password,
+    user_type
+) VALUES 
+(
+    'Setembrino Trocatapas',
+    'set@brino.com',
+    '1990-10-14',
+    'https://randomuser.me/api/portraits/lego/6.jpg',
+    'Programador, agricultor, enrolador, fazedor de nada.',
+    SHA1('Qw3rtyui0P'),
+    'author'
+),
+(
+    'Emegarda Sirineuza',
+    'emen@siri.com',
+    '2000-01-11',
+    'https://randomuser.me/api/portraits/lego/3.jpg',
+    'Cantora, compositora, arranjadora, arrumadora e operadora.',
+    SHA1('Qw3rtyui0P'),
+    'user'
+),
+(
+    'Eletricildo Chocante',
+    'eletro@choque.com',
+    '1981-05-02',
+    'https://randomuser.me/api/portraits/lego/5.jpg',
+    'Motorista, eletricista, ortopedista e horista.',
+    SHA1('Qw3rtyui0P'),
+    'admin'
+),
+(
+    'Juanita Malita',
+    'nitamalita@gmail.com',
+    '1991-10-12',
+    'https://randomuser.me/api/portraits/lego/1.jpg',
+    'Motorista, eletricista, ortopedista e horista.',
+    SHA1('Qw3rtyui0P'),
+    'author'
+),
+(
+    'Genesito Malito',
+    'genemalito@google.com',
+    '2000-11-01',
+    'https://randomuser.me/api/portraits/lego/2.jpg',
+    'Motorista, eletricista, ortopedista e horista.',
+    SHA1('Qw3rtyui0P'),
+    'author'
+);
 
-// Loop que obtém cada registro de '$res'
-while ($artigo = $res->fetch_assoc()) :
+-- Insere dados em 'articles'
+INSERT INTO articles (
+    art_title,
+    art_intro,
+    art_thumb,
+    art_content,
+    art_author
+) VALUES
+(
+    'Primeiro artigo',
+    'Como fazer alguma coisa que você não sabe fazer.',
+    'https://picsum.photos/100',
+    '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto fugiat, nesciunt iure porro aliquid id consequuntur nisi placeat assumenda vero magni repellendus possimus corporis sed, quaerat totam? Veniam, pariatur ex?</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore sunt quo nam commodi repudiandae voluptas excepturi est aut iste veniam. Itaque possimus ullam eius quaerat? Quam aliquam eius corporis ut!</p><img src="https://picsum.photos/400/300" alt="Imagem aleátória"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quisquam amet, sunt, magnam debitis aliquam voluptates eius veniam explicabo molestias enim illo quam exercitationem ducimus vel eos eligendi aut libero.</p><ul><li><a href="https://guithub.com/Luferat">GitHub do Fessô</a></li><li><a href="http://catabits.com.br">Blog do Fessô</a></li><li><a href="https://bit.ly/LuferatMeet">Google Meet do Fessô</a></li></ul><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At itaque deserunt perferendis sit voluptatum sunt, minus labore ratione modi nesciunt distinctio temporibus amet omnis sapiente, dicta repudiandae ipsum, eaque deleniti!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas quos quasi quis pariatur iure officia ab, eius beatae, fuga, in dolores neque possimus necessitatibus nostrum nulla expedita tempore harum tenetur?</p>',
+    '1'
+),
+(
+    'Veja como isso é feito',
+    'Aprenda a fazer da forma certa o que você sempre fez errado.',
+    'https://picsum.photos/101',
+    '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto fugiat, nesciunt iure porro aliquid id consequuntur nisi placeat assumenda vero magni repellendus possimus corporis sed, quaerat totam? Veniam, pariatur ex?</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore sunt quo nam commodi repudiandae voluptas excepturi est aut iste veniam. Itaque possimus ullam eius quaerat? Quam aliquam eius corporis ut!</p><img src="https://picsum.photos/400/300" alt="Imagem aleátória"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quisquam amet, sunt, magnam debitis aliquam voluptates eius veniam explicabo molestias enim illo quam exercitationem ducimus vel eos eligendi aut libero.</p><ul><li><a href="https://guithub.com/Luferat">GitHub do Fessô</a></li><li><a href="http://catabits.com.br">Blog do Fessô</a></li><li><a href="https://bit.ly/LuferatMeet">Google Meet do Fessô</a></li></ul><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At itaque deserunt perferendis sit voluptatum sunt, minus labore ratione modi nesciunt distinctio temporibus amet omnis sapiente, dicta repudiandae ipsum, eaque deleniti!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas quos quasi quis pariatur iure officia ab, eius beatae, fuga, in dolores neque possimus necessitatibus nostrum nulla expedita tempore harum tenetur?</p>',
+    '1'
+),
+(
+    'Comprando o que não pode ser vendido',
+    'Neste artigo aprenderemos a fazer uma coisa que ninguém faz.',
+    'https://picsum.photos/99',
+    '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto fugiat, nesciunt iure porro aliquid id consequuntur nisi placeat assumenda vero magni repellendus possimus corporis sed, quaerat totam? Veniam, pariatur ex?</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore sunt quo nam commodi repudiandae voluptas excepturi est aut iste veniam. Itaque possimus ullam eius quaerat? Quam aliquam eius corporis ut!</p><img src="https://picsum.photos/400/300" alt="Imagem aleátória"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quisquam amet, sunt, magnam debitis aliquam voluptates eius veniam explicabo molestias enim illo quam exercitationem ducimus vel eos eligendi aut libero.</p><ul><li><a href="https://guithub.com/Luferat">GitHub do Fessô</a></li><li><a href="http://catabits.com.br">Blog do Fessô</a></li><li><a href="https://bit.ly/LuferatMeet">Google Meet do Fessô</a></li></ul><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At itaque deserunt perferendis sit voluptatum sunt, minus labore ratione modi nesciunt distinctio temporibus amet omnis sapiente, dicta repudiandae ipsum, eaque deleniti!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas quos quasi quis pariatur iure officia ab, eius beatae, fuga, in dolores neque possimus necessitatibus nostrum nulla expedita tempore harum tenetur?</p>',
+    '1'
+);
 
-    // Formata HTML de saída
-    $artigos .= <<<HTML
-
-        <div class="item" onclick="location.href='/ler/?id={$artigo['art_id']}'">
-            <div class="thumb" style="background-image: url('{$artigo['art_thumb']}')" title="Imagem de {$artigo['art_title']}"></div>
-            <div class="body">
-                <h4>{$artigo['art_title']}</h4>
-                <span>{$artigo['art_intro']}</span>
-            </div>
-        </div>
-
-HTML;
-
-endwhile;
-
-/************************************************
- * Seus códigos PHP desta página terminam aqui! *
- ************************************************/
-
-/**
- * Variável que define o título desta página.
- */
-$title = "Quem tem fome tem pressa...";
-
-/**
- * Inclui o cabeçalho da página.
- */
-require($_SERVER['DOCUMENT_ROOT'] . '/_header.php');
-
-?>
-
-<section>
-
-    <div class="items">
-        <?php
-
-        // Exibe todos os artigos.
-        echo $artigos;
-
-        ?>
-    </div>
-
-</section>
-
-<aside>
-
-    <h3>Lateral</h3>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia, aperiam corporis culpa consequatur iusto.</p>
-
-</aside>
-
-<?php
-
-/**
- * Inclui o rodapé da página.
- */
-require($_SERVER['DOCUMENT_ROOT'] . '/_footer.php');
+-- Insere dados em 'articles'
+INSERT INTO articles (
+    art_title,
+    art_intro,
+    art_thumb,
+    art_content,
+    art_author
+) VALUES
+(
+    'É assim que se faz isso',
+    'Aprenda como fazer para apreder o que fazer, sabendo o que fazer.',
+    'https://picsum.photos/98',
+    '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto fugiat, nesciunt iure porro aliquid id consequuntur nisi placeat assumenda vero magni repellendus possimus corporis sed, quaerat totam? Veniam, pariatur ex?</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore sunt quo nam commodi repudiandae voluptas excepturi est aut iste veniam. Itaque possimus ullam eius quaerat? Quam aliquam eius corporis ut!</p><img src="https://picsum.photos/400/300" alt="Imagem aleátória"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quisquam amet, sunt, magnam debitis aliquam voluptates eius veniam explicabo molestias enim illo quam exercitationem ducimus vel eos eligendi aut libero.</p><ul><li><a href="https://guithub.com/Luferat">GitHub do Fessô</a></li><li><a href="http://catabits.com.br">Blog do Fessô</a></li><li><a href="https://bit.ly/LuferatMeet">Google Meet do Fessô</a></li></ul><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At itaque deserunt perferendis sit voluptatum sunt, minus labore ratione modi nesciunt distinctio temporibus amet omnis sapiente, dicta repudiandae ipsum, eaque deleniti!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas quos quasi quis pariatur iure officia ab, eius beatae, fuga, in dolores neque possimus necessitatibus nostrum nulla expedita tempore harum tenetur?</p>',
+    '4'
+),
+(
+    'A arte de fazer sem que se faça',
+    'Dessa forma que fazermos aquilo que deve ser feito sem que seja necessário fazer.',
+    'https://picsum.photos/102',
+    '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto fugiat, nesciunt iure porro aliquid id consequuntur nisi placeat assumenda vero magni repellendus possimus corporis sed, quaerat totam? Veniam, pariatur ex?</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore sunt quo nam commodi repudiandae voluptas excepturi est aut iste veniam. Itaque possimus ullam eius quaerat? Quam aliquam eius corporis ut!</p><img src="https://picsum.photos/400/300" alt="Imagem aleátória"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quisquam amet, sunt, magnam debitis aliquam voluptates eius veniam explicabo molestias enim illo quam exercitationem ducimus vel eos eligendi aut libero.</p><ul><li><a href="https://guithub.com/Luferat">GitHub do Fessô</a></li><li><a href="http://catabits.com.br">Blog do Fessô</a></li><li><a href="https://bit.ly/LuferatMeet">Google Meet do Fessô</a></li></ul><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At itaque deserunt perferendis sit voluptatum sunt, minus labore ratione modi nesciunt distinctio temporibus amet omnis sapiente, dicta repudiandae ipsum, eaque deleniti!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas quos quasi quis pariatur iure officia ab, eius beatae, fuga, in dolores neque possimus necessitatibus nostrum nulla expedita tempore harum tenetur?</p>',
+    '5'
+),
+(
+    'Cortes cortados de cantos arredondados',
+    'Neste artigo aprenderemos a fazer cantos de cortes cortados.',
+    'https://picsum.photos/103',
+    '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto fugiat, nesciunt iure porro aliquid id consequuntur nisi placeat assumenda vero magni repellendus possimus corporis sed, quaerat totam? Veniam, pariatur ex?</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore sunt quo nam commodi repudiandae voluptas excepturi est aut iste veniam. Itaque possimus ullam eius quaerat? Quam aliquam eius corporis ut!</p><img src="https://picsum.photos/400/300" alt="Imagem aleátória"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quisquam amet, sunt, magnam debitis aliquam voluptates eius veniam explicabo molestias enim illo quam exercitationem ducimus vel eos eligendi aut libero.</p><ul><li><a href="https://guithub.com/Luferat">GitHub do Fessô</a></li><li><a href="http://catabits.com.br">Blog do Fessô</a></li><li><a href="https://bit.ly/LuferatMeet">Google Meet do Fessô</a></li></ul><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At itaque deserunt perferendis sit voluptatum sunt, minus labore ratione modi nesciunt distinctio temporibus amet omnis sapiente, dicta repudiandae ipsum, eaque deleniti!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas quos quasi quis pariatur iure officia ab, eius beatae, fuga, in dolores neque possimus necessitatibus nostrum nulla expedita tempore harum tenetur?</p>',
+    '4'
+);
