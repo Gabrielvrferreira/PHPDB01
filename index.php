1720acf8158cba2f011eb05b0c9b38e36cffddcf
Gabrielvrferreira <99758660+Gabrielvrferreira@users.noreply.github.com>
2022-05-04 16:32:33 -0300

    565235

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+<<?php
 
 /**
  * Arquivo que faz a configuração incial da página.
@@ -9,46 +9,56 @@ require($_SERVER['DOCUMENT_ROOT'] . '/_config.php');
  * Seus códigos PHP desta página iniciam aqui! *
  ***********************************************/
 
-// Variável que armazena todos os artigos para exibição no HTML.
-$artigos = '';
+// Cria e inicializa as variáveis usadas no script
+$name = $email = $birth = $profile = $password = $error = '';
 
-// SQL que obtém todos os artigos.
-$sql = <<<SQL
+// Se formulário foi enviado...
+if ($_SERVER["REQUEST_METHOD"] == "POST") :
 
-SELECT art_id, art_title, art_intro, art_thumb 
-    FROM articles
-WHERE art_status = 'on'
-    AND art_date <= NOW()
-ORDER BY art_date DESC;
+    dump($_POST, false);                              // Dados do formulário
+    dump($_FILES, false);                             // Dados do arquivo enviado
+    dump(getimagesize($_FILES['photo']['tmp_name'])); // Dados da imagem
 
-SQL;
+    // Recebe o campo 'nome' do formulário e sanitiza
+    $name = trim(htmlspecialchars($_POST['name']));
 
-// Executa a query --> '$res' contém os artigos encontrados.
-$res = $conn->query($sql);
+    // Recebe o campo 'email' dor formulário e sanitiza
+    $email = trim(filter_var($_POST['email'], FILTER_SANITIZE_EMAIL));
 
-// Abre a lista de artigos.
-$artigos = '<div class="items">' . "\n";
+    // Recebe a data de nascimento e sanitiza
+    $birth = trim(htmlspecialchars($_POST['birth']));
 
-// Loop que obtém cada registro de '$res'
-while ($artigo = $res->fetch_assoc()) :
+    // Recebe o perfil e sanitiza
+    $profile = trim(htmlspecialchars($_POST['profile']));
 
-    // Formata HTML de saída
-    $artigos .= <<<HTML
+    // Recebe a senha e sanitiza
+    $password = trim(htmlspecialchars($_POST['password']));
 
-        <div class="item" onclick="location.href='/ler/?id={$artigo['art_id']}'">
-            <div class="thumb" style="background-image: url('{$artigo['art_thumb']}')" title="Imagem de {$artigo['art_title']}"></div>
-            <div class="body">
-                <h4>{$artigo['art_title']}</h4>
-                <span>{$artigo['art_intro']}</span>
-            </div>
-        </div>
+    // Verifica se tem algum campo vazio
+    if (
+        $name === '' or
+        $email === '' or
+        $birth === '' or
+        $profile === '' or
+        $password === ''
+    ) :
 
+        // Exibe mensagem de erro para o usuário e não faz mais nada
+        $error = <<<HTML
+
+            <h3>Oooops!</h3>
+            <p>Não foi possível enviar o cadastro.</p>
+            <p>Você precisa preencher todos os campos do formulário.</p>
+            
 HTML;
 
-endwhile;
+    elseif (preg_match('/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\S+$).{7,32}/', 'qw3rtyui0P')) :
 
-// Fecha a lista de artigos.
-$artigos .= '</div>';
+$error = 
+
+    endif;
+
+endif;
 
 /************************************************
  * Seus códigos PHP desta página terminam aqui! *
@@ -57,7 +67,7 @@ $artigos .= '</div>';
 /**
  * Variável que define o título desta página.
  */
-$title = "Quem tem fome tem pressa...";
+$title = "Cadastro...";
 
 /**
  * Inclui o cabeçalho da página.
@@ -68,12 +78,79 @@ require($_SERVER['DOCUMENT_ROOT'] . '/_header.php');
 
 <section>
 
-    <?php
+    <h2>Cadastre-se</h2>
 
-    // Exibe todos os artigos.
-    echo $artigos;
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" name="newuser" enctype="multipart/form-data">
 
-    ?>
+        <p>Preencha todos os campos com atenção para se cadastrar e ter acesso aos recursos exclusivos.</p>
+
+        <p>
+            <label for="name">Nome completo:</label>
+            <input type="text" name="name" id="name" required minlength="3" class="valid" autocomplete="off" value="Joca da Silva">
+        </p>
+
+        <p>
+            <label for="email">E-mail:</label>
+            <input type="email" name="email" id="email" required class="valid" autocomplete="off" value="joca@silva.com">
+        </p>
+
+        <p>
+            <label for="birth">Data de nascimento:</label>
+            <input type="date" name="birth" id="birth" required class="valid" autocomplete="off" value="2000-10-11">
+        </p>
+
+        <!-- p>
+            <label for="photo">Imagem de perfil:</label>
+            <input type="url" name="photo" id="photo" required class="valid" autocomplete="off" value="https://i.imgur.com/EVgxDw9.png">
+        <div class="form-help">
+            Dicas:
+            <ul>
+                <li>Publique a imagem na Web. Ex.: <a href="https://imgur.com/" target="_blank">imgUr</a></li>
+                <li>Use a imagem da sua rede social.</li>
+            </ul>
+        </div>
+        </p -->
+
+        <p>
+            <label for="photo">Imagem de perfil:</label>
+            <input type="file" name="photo" id="photo" required class="valid" accept="image/jpeg, image/jpg, image/png">
+        <div class="form-help">
+            <ul>
+                <li>Imagem quadrada no formato PNG ou JPG;</li>
+                <li>Tamanho mínimo de 64px x 64px x 5MB ;</li>
+                <li>Tamanho máximo de 515px x 512px x 5MB.</li>
+            </ul>
+        </div>
+        </p>
+
+        <p>
+            <label for="profile">Prefil resumido:</label>
+            <textarea name="profile" id="profile" required minlength="5" class="valid" autocomplete="off">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque dolore, ab voluptate, earum deleniti.</textarea>
+        <div class="form-help">
+            <ul>
+                <li>Escreva sobre você, de forma resumida.</li>
+            </ul>
+        </div>
+        </p>
+
+        <p>
+            <label for="password">Senha:</label>
+            <input type="password" name="password" id="password" required pattern="^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\S+$).{7,32}$" class="valid password" autocomplete="off" value="Qwertyui0p">
+            <button type="button" id="passToggle"><i class="fa-solid fa-eye fa-fw"></i></button>
+        <div class="form-help">
+            <ul>
+                <li>Mínimo de 7 e máximo de 32 caracteres;</li>
+                <li>Pelo menos uma letra maiúscula de A até Z;</li>
+                <li>Pelo menos um número de 0 à 9.</li>
+            </ul>
+        </div>
+        </p>
+
+        <p>
+            <button type="submit">Enviar</button>
+        </p>
+
+    </form>
 
 </section>
 
